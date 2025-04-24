43e533ea750bb4fb8ef63c71cc256d329784ae7b
Robertas <robervicsat@gmail.com>
2025-04-24 16:10:00 +0300

    updated gitignore

--- financeManager/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_financeManager_FILE C:/Users/P52s/Desktop/UNI/OOP(CPP)/KP1/komandinis-projektas_cpp_2_semestras/financeManager/build/Desktop-Debug/financeManager.exe)
-set(__QT_DEPLOY_TARGET_financeManager_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_financeManager_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Chartsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Cored.dll)
--- financeManager/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_financeManager_FILE C:/Users/Home/Desktop/mokslai/objektinis programavimas C++/komandinis-projektas_cpp_2_semestras/financeManager/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/financeManager.exe)
-set(__QT_DEPLOY_TARGET_financeManager_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_financeManager_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
